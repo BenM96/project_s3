b8338b276c85f296ed5333fb574164283ce8072b
Ben Mercer <smellmcguy@googlemail.com>
2021-02-07 18:55:20 +0000

    added cost calulation function. Currently very crude

--- Cost_calculations.go
+++ Cost_calculations.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
 
-func Get_bucket_cost (total_size_of_files int64) int{
+
+func Get_bucket_cost (total_size_of_files int64) float64{
+	//retuns the estimated cost of a bucket.
+
+	//convert total size of files to GB as it is what aws uses for pricing
+	total_size_of_files_GB := byte_conversion(total_size_of_files, "GB")
+	fmt.Println(total_size_of_files_GB)
 	
-	return 1
+	cost := float64(total_size_of_files_GB) * 0.024
+
+	return cost
 }
