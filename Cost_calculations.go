06961359b7092f41a3ee45d73179137167ead98b
Ben Mercer <smellmcguy@googlemail.com>
2021-02-07 22:04:55 +0000

    removed redundent println

--- Cost_calculations.go
+++ Cost_calculations.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 )
 
 
@@ -9,9 +9,7 @@ func Get_bucket_cost (total_size_of_files int64) float64{
 	//retuns the estimated cost of a bucket.
 
 	//convert total size of files to GB as it is what aws uses for pricing
-	total_size_of_files_GB := byte_conversion(total_size_of_files, "GB")
-	fmt.Println(total_size_of_files_GB)
-	
+	total_size_of_files_GB := byte_conversion(total_size_of_files, "GB")	
 	cost := float64(total_size_of_files_GB) * 0.024
 
 	return cost
